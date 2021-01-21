34af99207bc482edcab7814ef065a41c4f1f540a
lucasnethaj <lucasnethaj@gmail.com>
2021-01-21 15:02:02 +0100

    Initial Com

--- config.h
+++ config.h
@@ -4,9 +4,11 @@
 char shell[] = "/bin/sh";
 
 #define Super Mod4Mask
+#define Alt Mod1Mask
 
 enum {
   Music,
+  App,
   Power,
 
   // Declare modes above this
@@ -21,6 +23,10 @@ Key modes[MODE_SIZE][10] = {
     { 0, XK_l,       cmd("playerctl next") },
     { 0, XK_space,   cmd("playerctl play-pause") },
   },
+  [App] = {
+    { 0, XK_b,   cmd("firefox") },
+    { 0, XK_t,   cmd("st") },
+  } ,
   [Power] = {
     { 0, XK_s,       cmd("notify-send shutting_down") },
     { 0, XK_r,       cmd("notify-send restarting") },
@@ -29,13 +35,26 @@ Key modes[MODE_SIZE][10] = {
 
 // Define normal mode key bindings here
 Key keys[] = {
-  { Mod1Mask,  XK_y,             cmd("notify-send hello") },
-  { Mod1Mask,  XK_z,             mode(Music, True) },
-  { Mod1Mask,  XF86XK_PowerOff,  mode(Power, False) },
+  { Alt,  XK_z,             mode(Music, True) },
+  { Alt,  XK_a,             mode(App, True) },
+  { Alt,  XF86XK_PowerOff,  mode(Power, False) },
+
+  /* { XF86XK_AudioPlay,  cmd("playerctl play-pause") }, */
+  /* { XF86XK_AudioPrev,  cmd("playerctl previous") }, */
+  /* { XF86XK_AudioNext,  cmd("playerctl next") }, */
+  /* { XF86XK_AudioLowerVolume,  cmd("pactl set-sink-volume @DEFAULT_SINK@ -5%") }, */
+  /* { XF86XK_AudioRaiseVolume,  cmd("pactl set-sink-volume @DEFAULT_SINK@ +5%") }, */
+
+  { Super, XK_Pause,  cmd("playerctl play-pause") },
+  { Super, XK_Home,  cmd("playerctl previous") },
+  { Super, XK_End,  cmd("playerctl next") },
+  { Super, XK_Next,  cmd("pactl set-sink-volume @DEFAULT_SINK@ -5%") },
+  { Super, XK_Prior,  cmd("pactl set-sink-volume @DEFAULT_SINK@ +5%") },
 };
 
 ModeProperties mode_properties[MODE_SIZE] = {
   [Music] = { "Music player" },
+  [App] = { "Applications" },
   [Power] = { "Power menu" },
 };
 
